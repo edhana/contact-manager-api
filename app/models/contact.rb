bceb0520a52e8967692bc3d52e40adc3bd8de657
Eduardo Marques <edhana@gmail.com>
2017-08-20 22:15:43 -0300

    Added the CORS configuration

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,8 @@ class Contact < ApplicationRecord
   validates :firstname, presence: true
   validates :lastname, presence: true
   validates :email, presence: true
+
+  def Contact.find_by_token(token_id)
+    return Contact.find(token_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resource :contacts, only: [:create, :update, :destroy]
       get "/hello", to: "contacts#hello"
+      post "/token", to: "contacts#find_by_token"
     end
   end
 end
