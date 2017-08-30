23a8c46a6d6ad1549af6100192005f4b22f6e35e
Eduardo Marques <edhana@gmail.com>
2017-08-30 16:06:41 -0300

    Implemented missing tests for the contacts_controller_spec (API) and added new tests to the find by token action

--- spec/controllers/api/v1/contacts_controller_spec.rb
+++ spec/controllers/api/v1/contacts_controller_spec.rb
@@ -23,11 +23,13 @@ RSpec.describe Api::V1::ContactsController, type: :controller do
     it 'returns http success' do
       post :create, params: { contact: Contact.new(valid_contact).as_json }
       expect(response).to have_http_status(:success)
+      expect(response.content_type).to eq 'application/json'
     end
 
     it 'return unprocessable entity for an invalid contact' do
       post :create, params: { contact: Contact.new(invalid_contact).as_json }
       expect(response).to have_http_status(:unprocessable_entity)
+      expect(response.content_type).to eq 'application/json'
     end
   end
 
@@ -35,11 +37,13 @@ RSpec.describe Api::V1::ContactsController, type: :controller do
     it 'returns http success' do
       put :update, params: { id: @contact.id, contact: @contact.as_json }
       expect(response).to have_http_status(:success)
+      expect(response.content_type).to eq 'application/json'
     end
 
     it 'return unprocessable entity for contact without id' do
       put :update, params: { contact: @contact.as_json }
       expect(response).to have_http_status(:unprocessable_entity)
+      expect(response.content_type).to eq 'application/json'
     end
   end
 
@@ -47,15 +51,32 @@ RSpec.describe Api::V1::ContactsController, type: :controller do
     it 'returns http success' do
       delete :destroy, params: { id: @contact.id }
       expect(response).to have_http_status(:success)
+      expect(response.content_type).to eq 'application/json'
     end
 
     it 'return unprocessable entity for request without id' do
       delete :destroy
       expect(response).to have_http_status(:unprocessable_entity)
+      expect(response.content_type).to eq 'application/json'
     end
   end
 
   describe 'GET #token' do
-    it 'returns a contact using token id'
+    it 'returns a contact using token id' do
+      get :find_by_token, params: { token_id: @contact.id }
+      expect(response).to have_http_status(:success)
+      expect(response.content_type).to eq 'application/json'
+    end
+
+    it 'return unprocessable entity without a token id parameter' do
+      expect { get ':find_by_token', params: {} }.to(
+        raise_error(ActionController::UrlGenerationError)
+      )
+    end
+
+    it 'return unprocessable entity with an invalid token id' do
+      get :find_by_token, params: { token_id: 10_000_011 }
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
   end
 end
