d29b0222eeeef267d5eb6fd8aae983b51c38e741
Eduardo Marques <edhana@gmail.com>
2017-08-30 16:07:38 -0300

    Refactored the code using rails lint checker Rubocop

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  let(:valid_contact){
-    {:firstname => Faker::Name.first_name,
-    :lastname => Faker::Name.last_name,
-    :email => Faker::Internet.email}
-  }
+  let(:valid_contact) do
+    { firstname: Faker::Name.first_name,
+      lastname: Faker::Name.last_name,
+      email: Faker::Internet.email }
+  end
 
-  it "with valid_attributes is saved" do
-    expect{
+  it 'with valid_attributes is saved' do
+    expect do
       Contact.create(valid_contact)
-    }.to change(Contact, :count).by(1)
+    end.to change(Contact, :count).by(1)
   end
 
-  it "with invalid attributes is not saved" do
-    expect{Contact.create(invalid_contact).not_to change(Contact, :count).by(1)}
+  it 'with invalid attributes is not saved' do
+    expect { Contact.create(invalid_contact).not_to change(Contact, :count).by(1) }
   end
 
-  it "use token as search parameter"
+  it 'use token as search parameter'
 end
